e2eac7e8dd97d9fa28cd78d673cf0875e63a811b
Rafael <rafael.mendoza@telecom.pt>
2018-11-18 15:56:19 -0200

    Initial Commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
+var app = angular.module('todo-app', ['ionic', 'LocalStorageModule'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -23,4 +22,72 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function(localStorageServiceProvider){
+  localStorageServiceProvider.setPrefix('todo-app');
+});
+
+app.controller('main', function ($scope, $ionicModal, localStorageService){
+
+  var taskData = [ {content:'task', title: 'teste'}, {content:'task2', title: 'teste2'} ];
+  localStorageService.set(taskData, taskData);
+
+  $scope.tasks = [];
+
+  $scope.task = {  };
+
+  $ionicModal.fromTemplateUrl('templates/new-task-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function (modal) {
+    // alert("Abrindo modal");
+    $scope.newTaskModal = modal;
+  });
+  
+  $scope.getTasks = function () {
+    // alert("getting tasks");
+    if(localStorageService.get(taskData)) {
+      $scope.tasks = localStorageService.get(taskData);
+      console.log($scope.tasks);
+    } else {
+      $scope.tasks = [];
+    }
+  }
+
+  $scope.openTaskModal = function () {
+    $scope.newTaskModal.show();
+  }
+
+  $scope.closeTaskModal = function () {
+    $scope.newTaskModal.hide();
+  }
+
+  $scope.createTask = function () {
+    // alert("creating task");
+    console.log($scope.task);
+    $scope.tasks.push($scope.task);
+    localStorageService.set(taskData, $scope.task);
+    $scope.task = {};
+
+    $scope.newTaskModal.hide();
+  }
+
+  $scope.removeTask = function (index) {
+    // alert("removing task");
+    console.log($scope.tasks);
+    $scope.tasks.splice(index,1);
+    localStorageService.set(taskData, $scope.tasks);
+  }
+
+  $scope.completeTask = function (index) {
+    // alert("completing task");
+    if(index !== 1)
+    {
+      $scope.tasks[index].completed = true;
+    }
+
+    localStorageService.set(taskData, $scope.tasks);
+  }
+
+});
